6515a3a7f7e8104e61083b1ca381e836a8526d92
soccert <talyagriffin2004@gmail.com>
2018-07-26 19:52:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog
+  
+ def initialize(name)
+    @name=name
+  end
+  
+  
+  
+  def name 
+    @name
+  end
+  
+end
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
